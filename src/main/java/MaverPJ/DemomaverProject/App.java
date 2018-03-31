e599c892257db9ff0d60c7ea5a4b010da2caa34f
vaggi smith <opusminds1@gmail.com>
2018-04-01 01:11:43 +0530

    First Commit

---
+++ src/main/java/MaverPJ/DemomaverProject/App.java
@@ -0,0 +1,17 @@
+package MaverPJ.DemomaverProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void hello(){
+    	System.out.println( "Hellos World!" );
+    }
+}
---
+++ src/test/java/MaverPJ/DemomaverProject/chromeTest.java
@@ -0,0 +1,39 @@
+package MaverPJ.DemomaverProject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class chromeTest {
+	
+	private WebDriver driver;
+
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		// Your test code here
+		System.out.println("I am in test");
+	}
+
+}
